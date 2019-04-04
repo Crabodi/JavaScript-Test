22f5556e643473923bab3feefec0d9d7d22066fb
Crabodi <crabodi@gmail.com>
2019-04-04 11:14:06 +0500

    Это наш первый комит

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+
+
+// let answer = confirm("What a fuck");
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(--decr);
